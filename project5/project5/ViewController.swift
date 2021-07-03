0a1fbbbfb4bf6da8d9808cabc99fa35247384921
robinvanmeeuwen <robinvanmeeuwen@gmail.com>
2021-07-03 23:22:19 +0200

    project 5 begonnen

---
+++ project5/project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project5
+//
+//  Created by Robin Van meeuwen on 03/07/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
