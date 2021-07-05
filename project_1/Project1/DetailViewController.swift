e4a2fd555ba373d49f2182340144a59e5e10574d
robinvanmeeuwen <robinvanmeeuwen@gmail.com>
2021-07-05 13:21:16 +0200

    project 1 fix

---
+++ project_1/Project1/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//
+//  Created by Robin van Meeuwen on 10/06/21.
+//  Copyright © 2021 Robin van Meeuwen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+	@IBOutlet var imageView: UIImageView!
+	var selectedImage: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(recommendTapped))
+
+		if let imageToLoad = selectedImage {
+			imageView.image  = UIImage(named: imageToLoad)
+		}
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func recommendTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }}
