bb7609e52977be65b98d82c59e227c89bb5a579e
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-06 14:04:34 +0200

    project 10 bijgewerkt

---
+++ project10/project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Robin van Meeuwen on 06/09/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var ImageView: UIImageView!
+    @IBOutlet var Name: UILabel!
+    
+}
