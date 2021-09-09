649b5eb0f760fe52fe1f51adb68348998ac19c75
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-09 15:24:15 +0200

    project 13

---
+++ project13/project13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Robin van Meeuwen on 09/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    
+    
+
+}
+
