99b486c3cd5bf21ec8fabc0d9df5febd18ea2c82
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-09 14:15:01 +0200

    project 13 toegevoegd

---
+++ challenge1/challenge1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  challenge1
+//
+//  Created by Robin van Meeuwen on 09/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = UserDefaults.standard
+        
+        defaults.set(<#T##value: Any?##Any?#>, forKey: <#T##String#>)
+    }
+
+
+}
+
