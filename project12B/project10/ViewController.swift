145a489c36ebd558a9bbab2ab75fae040d3b0b3b
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-09 11:45:32 +0200

    project 12 afgerond

--- project12B/project10/ViewController.swift
+++ project12B/project10/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("failed to load people")
+            }
+        }
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -57,6 +69,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        save()
         collectionView?.reloadData()
 
         dismiss(animated: true)
@@ -70,11 +83,11 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
 
-        ac.addAction(UIAlertAction(title: "OK", style: .default) { [unowned self, ac] _ in
-            let newName = ac.textFields![0]
-            person.name = newName.text!
-
-            self.collectionView?.reloadData()
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let newName = ac?.textFields![0].text else {return}
+            person.name = newName
+            self?.save()
+            self?.collectionView?.reloadData()
         })
 
         present(ac, animated: true)
@@ -84,5 +97,14 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
+    }
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save cards")
+        }
     }
 }
