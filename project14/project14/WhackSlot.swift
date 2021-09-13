4cd1da2c180d302d992d7b8ee385ffd2948d5525
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-13 12:34:22 +0200

    project 14 afgemaakt

--- project14/project14/WhackSlot.swift
+++ project14/project14/WhackSlot.swift
@@ -23,22 +23,25 @@ class WhackSlot: SKNode {
         cropNode.position = CGPoint(x: 0, y: 15)
         cropNode.zPosition = 1
         cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
-
+        
         charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
         charNode.name = "character"
         cropNode.addChild(charNode)
-
+        
         addChild(cropNode)
     }
     
     func show(hideTime: Double) {
         if isVisible { return }
-
+        
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
-
+        
         if Int.random(in: 0...2) == 0 {
             charNode.texture = SKTexture(imageNamed: "penguinGood")
             charNode.name = "charFriend"
@@ -46,15 +49,24 @@ class WhackSlot: SKNode {
             charNode.texture = SKTexture(imageNamed: "penguinEvil")
             charNode.name = "charEnemy"
         }
-    
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
             self?.hide()
         }
     }
     func hide() {
         if !isVisible { return }
-
+        
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
     }
+    
+    func hit() {
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction .moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [weak self] in self?.isVisible = false}
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
+}
 }
