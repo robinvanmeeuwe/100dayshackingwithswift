2972152cbcf56c889ecbac2006ba5d7a84fd3348
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-14 15:25:51 +0200

    project 17 afgerond

--- project17/project17/GameScene.swift
+++ project17/project17/GameScene.swift
@@ -6,6 +6,7 @@
 //
 
 import SpriteKit
+import UIKit
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -22,34 +23,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+        
+    
     override func didMove(to view: SKView) {
-        backgroundColor = .black
         
-        starfield = SKEmitterNode(fileNamed: "starfield" )
-        starfield.position = CGPoint(x: 1024, y: 384)
-        starfield.advanceSimulationTime(10)
-        addChild(starfield)
-        starfield.zPosition = -1
         
-        player = SKSpriteNode(imageNamed: "player")
-        player.position = CGPoint(x: 100, y: 384)
-        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
-        player.physicsBody?.contactTestBitMask = 1
-        addChild(player)
         
-        scorelabel = SKLabelNode(fontNamed: "Chalkduster")
-        scorelabel.position = CGPoint(x: 16, y: 16)
-        scorelabel.horizontalAlignmentMode = .left
-        addChild(scorelabel)
+        super.didMove(to: view)
         
-        score = 0
         
-        physicsWorld.gravity = .zero
-        physicsWorld.contactDelegate = self
-        
-        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
-        
+            backgroundColor = .black
+            
+            starfield = SKEmitterNode(fileNamed: "starfield")!
+            starfield.position = CGPoint(x: 1024, y: 384)
+            starfield.advanceSimulationTime(10)
+            addChild(starfield)
+            starfield.zPosition = -1
+            
+            player = SKSpriteNode(imageNamed: "player")
+            player.position = CGPoint(x: 100, y: 384)
+            player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+            player.physicsBody?.contactTestBitMask = 1
+            addChild(player)
+            
+            scorelabel = SKLabelNode(fontNamed: "Chalkduster")
+            scorelabel.position = CGPoint(x: 16, y: 16)
+            scorelabel.horizontalAlignmentMode = .left
+            addChild(scorelabel)
+            
+            score = 0
+            
+            physicsWorld.gravity = .zero
+            physicsWorld.contactDelegate = self
+            
+            gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+       
+
+            
+       
     }
+    
     @objc func createEnemy() {
             guard let enemy = possibleEnemies.randomElement() else {return}
             
@@ -64,9 +77,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         sprite.physicsBody?.linearDamping = 0
         sprite.physicsBody?.angularDamping = 0
         }
-        
     
- 
+
     override func update(_ currentTime: TimeInterval) {
         for node in children {
             if node.position.x < -300 {
@@ -77,5 +89,53 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if !isGameOver {
             score += 1
         }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        var location = touch.location(in: self)
+        
+        if location.y < 100 {
+            location.y = 100
+        }else if location.y > 668 {
+            location.y = 668
+        }
+        player.position = location
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion" )!
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        isGameOver = true
+        gameTimer?.invalidate()
+        
+        let gameOverLabel = SKLabelNode(fontNamed: "Helvetica")
+        gameOverLabel.name = "gameOverLabel"
+        gameOverLabel.fontSize = 30
+        gameOverLabel.text = "Game Over! Tap to play again."
+        gameOverLabel.position = CGPoint(x: 512, y: 387)
+        gameOverLabel.zPosition = 1
+        gameOverLabel.isUserInteractionEnabled = true
+        addChild(gameOverLabel)
+        
+        restartGame()
+        
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    func restartGame() {
+        removeAllChildren()
+  //      startGame()
     }
 }
