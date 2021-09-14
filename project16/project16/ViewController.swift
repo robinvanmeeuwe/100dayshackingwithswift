e9fa3cd941439e5e8be5f94516516f13033282b3
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-14 12:45:50 +0200

    project 16 af challenge buit nog

--- project16/project16/ViewController.swift
+++ project16/project16/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
+    enum maptype:NSInteger
+       {
+           case standardmap = 0
+           case satellitemap = 1
+           case hybridmap = 2
+       }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "home to 2012 summer olympics")
+        let oslo = Capital(title: "Olslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "founded over thousand years ago")
+        let Paris = Capital(title: "paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "City of Love")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "has a whole country inside it")
+        let washington = Capital(title: "Washington", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "named after george himself")
+        
+        mapView.addAnnotations([london, oslo, Paris, rome, washington])
+        
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Capital else { return nil }
+        
+        let identifier = "Capital"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+            
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else {return}
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default ))
+        present(ac, animated: true)
+        
+    }
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBAction func segmentedControlAction(sender: UISegmentedControl!) {
+        switch (sender.selectedSegmentIndex) {
+            case 0:
+                mapView.mapType = .standard
+            case 1:
+                mapView.mapType = .satellite
+            default:
+                mapView.mapType = .hybrid
+        }
+        
+        
+    }
+    
+    }
+    
+    
 
 
-}
+
 
