cad22517370a75e7dec772e661825f225757510c
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-20 14:08:15 +0200

    project 20 dag 1 en 2

--- project20/project20/GameScene.swift
+++ project20/project20/GameScene.swift
@@ -9,11 +9,65 @@ import SpriteKit
 
 
 class GameScene: SKScene {
+    var gametimer:Timer?
+    var fireworks = [SKNode]()
     
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    var score = 0 {
+        didSet {
+            
+        }
+    }
 
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint (x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+
         
     }
+    
+    func createFirework(xMovement: CGFloat, x:Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+        default:
+            firework.color = .red
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        if let emitter = SKEmitterNode(fileNamed: "fuse") {
+                emitter.position = CGPoint(x: 0, y: -22)
+                node.addChild(emitter)
+            }
+
+            
+            fireworks.append(node)
+            addChild(node)
+        }
+    
+
 }
