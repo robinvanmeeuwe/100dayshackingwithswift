f9a539db073ef079db98d88924925f35e1fbd8db
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-21 15:43:41 +0200

    project 21 part 1,2

--- project21/project21/ViewController.swift
+++ project21/project21/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -16,11 +17,37 @@ class ViewController: UIViewController {
     }
 
     @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
         
+        center.requestAuthorization(options: [.alert, .badge, .sound]) {
+            granted, error in if granted {
+                print("gg")
+            } else {
+                print("nope")
+            }
+        }
     }
     
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
         
+        let content = UNMutableNotificationContent()
+        content.title = "late wake up call"
+        content.body = "the early bird catches the worm, but the second mouse gets the cheese"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+        
+        let trigger =  UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
     
 }
