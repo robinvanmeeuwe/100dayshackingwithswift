3c39cb53689281df140bf9caea7046eb06dc86c7
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-21 14:11:26 +0200

    project 21 toegevoegd

---
+++ project21/project21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project21
+//
+//  Created by Robin van Meeuwen on 21/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
