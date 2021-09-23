105b521ee988047cef62bbcd7bdf0ff1f20cf9e4
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-23 10:27:40 +0200

    project22 test laptop

--- project22/project22/ViewController.swift
+++ project22/project22/ViewController.swift
@@ -5,13 +5,30 @@
 //  Created by Robin van Meeuwen on 23/09/2021.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var dinstanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    //do stuff
+                }
+            }
+        }
     }
 
 
