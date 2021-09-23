92c2c5e4591088f01392533c975e90cde88a3459
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-23 16:04:39 +0200

    project 23

--- project23/project23/GameScene.swift
+++ project23/project23/GameScene.swift
@@ -9,11 +9,38 @@ import SpriteKit
 
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
     
-    
+    var score = 0 {
+        didSet{
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    func createScore(){
+        
+    }
+    
+    func createLives() {
+        
+    }
+    
+    func createSlices() {
         
     }
 }
