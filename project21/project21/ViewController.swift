b91b5031c2f9c5847f3ac23e62c3de4e7b566d71
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-23 09:34:14 +0200

    project22 toegevoegd

--- project21/project21/ViewController.swift
+++ project21/project21/ViewController.swift
@@ -85,3 +85,4 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
     
 }
 
+  
---
+++ project22/project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project22
+//
+//  Created by Robin van Meeuwen on 23/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
