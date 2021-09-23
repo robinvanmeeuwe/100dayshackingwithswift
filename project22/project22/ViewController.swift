23d115e5f9ebd25bcfbf41670c3a38f9af18cd27
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-23 14:21:19 +0200

    project 22 test

--- project22/project22/ViewController.swift
+++ project22/project22/ViewController.swift
@@ -33,10 +33,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func startScanning() {
         let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
         
         locationManager?.startMonitoring(for: beaconRegion)
         locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) {
+            switch distance {
+           
+            case .far:
+                self.view.backgroundColor = .blue
+                self.dinstanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = .orange
+                self.dinstanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.dinstanceReading.text = "RIGHT HERE"
+                
+            default:
+                self.view.backgroundColor = .gray
+                self.dinstanceReading.text = "UNKOWN"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+               update(distance: beacon.proximity)
+           } else {
+               update(distance: .unknown)
+           }
     }
 
 
