5c7d02820b7c30d840571f57389dcec262df17be
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-24 10:34:06 +0200

    project 23 part 2

--- project23/project23/GameScene.swift
+++ project23/project23/GameScene.swift
@@ -17,6 +17,11 @@ class GameScene: SKScene {
         }
     }
     
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -32,15 +37,40 @@ class GameScene: SKScene {
         createSlices()
     }
     
-    func createScore(){
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
         
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
     }
     
     func createLives() {
-        
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
     }
     
     func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceFG)
+        addChild(activeSliceBG)
         
     }
 }
