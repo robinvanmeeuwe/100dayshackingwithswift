79713dcf9d83d3f4c5d507658cd2af56d88d34b5
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-28 15:52:43 +0200

    project 24 toegevoegd

--- project24/project24.swift
+++ project24/project24.swift
@@ -1,1 +1,10 @@
 import UIKit
+
+let name = "Taylor"
+for letter in name {
+    print("give me a \(letter)!"
+          
+        
+}
+          
+          let letter = name[name.index(name.startIndex, offsetBy: 3)]
---
+++ project24_1.playground/Contents.swift
@@ -0,0 +1,86 @@
+import UIKit
+
+let name = "Taylor"
+for letter in name {
+    print("give me a \(letter)!")
+}
+          
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
+
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("345")
+
+
+extension String {
+    // remove a prefix if it exists
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+
+    // remove a suffix if it exists
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+let input = "swift is like objective-c without the c"
+input.contains("swift")
+
+let languages = ["pyhton", " Ruby", "swift"]
+languages.contains("swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        return false
+    }
+}
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.black,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+//let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+extension String {
+    func withPrefix(_ withPrefix: String) -> {
+        let str = "Hello! Swift"
+        str.prefix(100)    }
+}
