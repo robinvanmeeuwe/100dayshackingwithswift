0cd3e2b106d5104e34053f6f12b42fcd4a15ef75
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-09-28 16:17:21 +0200

    project 25 toegevoegd en eerste challenge project 24

---
+++ project25/project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project25
+//
+//  Created by Robin van Meeuwen on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
