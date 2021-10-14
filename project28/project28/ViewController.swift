2eb56be0a9c6ddd8463fe3dad6286ad584988af1
Robin van Meeuwen werk <robin.m@socialdeal.nl>
2021-10-14 11:53:24 +0200

    project 28 afgerond

--- project28/project28/ViewController.swift
+++ project28/project28/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Robin van Meeuwen on 11/10/2021.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -24,30 +27,53 @@ class ViewController: UIViewController {
     
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-
+        
         let keyboardScreenEndFrame = keyboardValue.cgRectValue
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-
+        
         if notification.name == UIResponder.keyboardWillHideNotification {
             secret.contentInset = .zero
         } else {
             secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
         }
-
+        
         secret.scrollIndicatorInsets = secret.contentInset
-
+        
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
     }
     @IBAction func authenticateTapped(_ sender: Any) {
-        unlockSecretMessage()
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {[weak self] success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified, please  try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                        
+                    }
+                    
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry", message: "Your device is not configured for biometric autentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     func unlockSecretMessage() {
         secret.isHidden = false
         title = "secret stuff"
         
         secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ??
-            ""
+        ""
     }
     
     @objc func saveSecretMessage() {
@@ -58,6 +84,6 @@ class ViewController: UIViewController {
         secret.isHidden = true
         title = "nothing to see here"
     }
-
+    
 }
 
